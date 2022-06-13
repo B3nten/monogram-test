69605e86874c0e8910c1b04d1505f53feba67674
benten <ben@benten.ca>
2022-06-12 19:06:29 -0600

    Add responsive cards to hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,26 @@ export default function Home() {
             jamstack
             <span className='bg-[url(/assets/gradient.webp)] bg-[length:300px_200px] sm:bg-[length:500px_450px] lg:bg-[length:800px_600px] bg-right-top bg-clip-text text-transparent'> 101</span>
           </h1>
-          <p className='md:ml-[12vw] 2xl:ml-44 mt-11 text-sm sm:text-base lg:text-lg max-w-md sm:max-w-[549px] text-brand-gray'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.
-          </p>
-          <img className='w-[10vw] lg:w-auto mt-24 md:ml-[24vw] 2xl:ml-[372]' src='/assets/mouseline.svg' />
-        </div>
-        <div>
-          <img className='w-[280px] h-[629px] object-cover object-right-top rounded-[68px] ' src='/assets/gradient.webp' />
-          <img className='w-[280px] h-[629px] object-cover object-bottom rounded-[68px] ' src='/assets/gradient.webp' />
+          <div className='grid grid-cols-2 mt-7 gap-4 lg:gap-8'>
+            <div>
+              <p className='md:ml-[12vw] 2xl:ml-44 mt-5 text-sm sm:text-base lg:text-lg max-w-md sm:max-w-[549px] text-brand-gray'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.
+              </p>
+              <img className='hidden [@media_screen_and_(min-width:430px)]:block w-[10vw] lg:w-auto mt-24 mx-auto' src='/assets/mouseline.svg' />
+            </div>
+            <div className=''>
+              <div className='flex space-x-[3vw] 2xl:space-x-[46px] [@media_screen_and_(max-width:1350px)]:mt-32 [@media_screen_and_(max-width:700px)]:mt-12'>
+                <img className='w-[18vw] h-[42vw] 2xl:w-[280px] 2xl:h-[629px] rounded-[5vw] 2xl:rounded-[68px] ' src='/assets/rectangle01.webp' />
+                <img className='w-[18vw] h-[44vw] 2xl:w-[280px] 2xl:h-[670px] rounded-[5vw] 2xl:rounded-[68px] mt-[-11vw] 2xl:mt-[-171px]' src='/assets/rectangle02.webp' />
+              </div>
+              {/* hide blur cards if unsupported */}
+              <div className='flex space-x-[2.5vw] 2xl:space-x-[40px] ml-[10vw] 2xl:ml-40 opacity-0 [@supports(backdrop-filter:blur(0))]:opacity-100'>
+                <div className='w-[18vw] h-[32vw] 2xl:w-[280px] 2xl:h-[479px] rounded-[5vw] 2xl:rounded-[68px] mt-[-20vw] 2xl:mt-[-300px] shrink-0 bg-white/30 backdrop-blur-2xl' />
+                <div className='w-[18vw] h-[44vw] 2xl:w-[280px] 2xl:h-[662px] rounded-[5vw] 2xl:rounded-[68px] mt-[-33vw] 2xl:mt-[-500px] shrink-0 bg-white/30 backdrop-blur-2xl' />
+              </div>
+            </div>
+            <img className='[@media_screen_and_(min-width:430px)]:hidden col-span-2 w-[50px] mx-auto mt-[-2rem]' src='/assets/mouseline.svg' />
+          </div>
         </div>
       </section>
 
