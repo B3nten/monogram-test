bd5b22434471d1439d3c5e086d7df3c41aeee71a
benten <ben@benten.ca>
2022-06-13 16:53:01 -0600

    Make responsiveness changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@
 export default function Home() {
   return (
     <>
-
       {/* Hero section */}
       <section>
-        <div className="flex flex-col items-start mt-10 md:mt-20 lg:mt-36 p-1 sm:px-2 md:px-0">
+        {/* Desktop */}
+        <div className="hidden sm:flex flex-col items-start mt-10 md:mt-20 lg:mt-36 p-1 sm:px-2 md:px-0">
           <h1 className='md:ml-[8vw] 2xl:ml-32 text-5xl sm:text-8xl lg:text-[133px] font-black tracking-tight leading-[1.15]'>
             jamstack
             <span className='bg-[url(/assets/gradient.webp)] bg-[length:300px_200px] sm:bg-[length:500px_450px] lg:bg-[length:800px_600px] bg-right-top bg-clip-text text-transparent'> 101</span>
@@ -22,7 +22,6 @@ export default function Home() {
                 <img className='w-[18vw] h-[42vw] 2xl:w-[280px] 2xl:h-[629px]' src='/assets/rectangle01.webp' />
                 <img className='w-[18vw] h-[44vw] 2xl:w-[280px] 2xl:h-[670px] mt-[-11vw] 2xl:mt-[-171px]' src='/assets/rectangle02.webp' />
               </div>
-              {/* hide blur cards if unsupported */}
               <div className='flex space-x-[2.5vw] 2xl:space-x-[40px] ml-[10vw] 2xl:ml-40 opacity-0 [@supports(backdrop-filter:blur(0))]:opacity-100'>
                 <div className='w-[18vw] h-[32vw] 2xl:w-[280px] 2xl:h-[479px] rounded-[5vw] 2xl:rounded-[68px] mt-[-20vw] 2xl:mt-[-300px] shrink-0 bg-white/30 backdrop-blur-2xl' />
                 <div className='w-[18vw] h-[44vw] 2xl:w-[280px] 2xl:h-[662px] rounded-[5vw] 2xl:rounded-[68px] mt-[-33vw] 2xl:mt-[-500px] shrink-0 bg-white/30 backdrop-blur-2xl' />
@@ -31,20 +30,44 @@ export default function Home() {
             <img className='[@media_screen_and_(min-width:430px)]:hidden col-span-2 w-[50px] mx-auto mt-[-2rem]' src='/assets/mouseline.svg' />
           </div>
         </div>
+        {/* Mobile View */}
+        <div className='p-2 sm:hidden'>
+          <h1 className='text-7xl sm:text-8xl lg:text-[133px] font-black tracking-tight leading-[1.15]'>
+            jamstack
+          </h1>
+          <div className="grid grid-cols-2 gap-4">
+            <div><span className='text-8xl font-black bg-[url(/assets/gradient.webp)] bg-[length:300px_200px] sm:bg-[length:500px_450px] lg:bg-[length:800px_600px] bg-right-top bg-clip-text text-transparent'> 101</span>
+              <p className='md:ml-[12vw] 2xl:ml-44 mt-5 text-sm sm:text-base lg:text-lg max-w-md sm:max-w-[549px] text-brand-gray'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.
+              </p>
+            </div>
+            <div className='hidden'>
+              <div className='flex space-x-[3vw] 2xl:space-x-[46px] [@media_screen_and_(max-width:1350px)]:mt-32 [@media_screen_and_(max-width:700px)]:mt-12'>
+                <img className='w-[18vw] h-[42vw] 2xl:w-[280px] 2xl:h-[629px]' src='/assets/rectangle01.webp' />
+                <img className='w-[18vw] h-[44vw] 2xl:w-[280px] 2xl:h-[670px] mt-[-11vw] 2xl:mt-[-171px]' src='/assets/rectangle02.webp' />
+              </div>
+              <div className='flex space-x-[2.5vw] 2xl:space-x-[40px] ml-[10vw] 2xl:ml-40 opacity-0 [@supports(backdrop-filter:blur(0))]:opacity-100'>
+                <div className='w-[18vw] h-[32vw] 2xl:w-[280px] 2xl:h-[479px] rounded-[5vw] 2xl:rounded-[68px] mt-[-20vw] 2xl:mt-[-300px] shrink-0 bg-white/30 backdrop-blur-2xl' />
+                <div className='w-[18vw] h-[44vw] 2xl:w-[280px] 2xl:h-[662px] rounded-[5vw] 2xl:rounded-[68px] mt-[-33vw] 2xl:mt-[-500px] shrink-0 bg-white/30 backdrop-blur-2xl' />
+              </div>
+            </div>
+          </div>
+          <img className='col-span-2 w-[30px] mx-auto mt-[2rem]' src='/assets/mouseline.svg' />
+        </div>
       </section>
 
       {/* Javascript computer section */}
       <section className='mt-10'>
         <div className="flex flex-col items-center px-4 space-y-[5vw] sm:space-y-10 md:space-y-12 lg:space-y-18 xl:space-y-20">
-          <img className='absolute -z-10' src='/assets/orange_square.webp' />
+          <div className="absolute -z-10 px-2 md:px-4 2xl:px-0"><img className='' src='/assets/orange_square.webp' /></div>
           <h2 className='text-white text-4xl md:text-6xl lg:text-8xl xl:text-[133px] font-black'>javascript</h2>
           <p className='text-sm sm:text-lg text-white max-w-md md:max-w-[586px] md:leading-loose font-light'>Statically generated pages are brought to life with client-side JavaScript libraries and frameworks, such as Algolia and Next.js.</p>
           <img className='w-[50vw] md:w-[60vw] xl:w-auto' src='/assets/computer.webp' />
         </div>
-      </section>
+      </section>;
 
       {/* Logo section */}
-      <div className="relative bg-brand-bg mt-[-10rem] sm:mt-[-20rem] lg:mt-[-28rem] xl:mt-[-32rem] -z-30">
+      <div className="relative bg-brand-bg mt-[-15rem] sm:mt-[-20rem] md:mt-[-28rem] lg:mt-[-32rem] xl:mt-[-38rem] -z-30">
         <section>
 
           {/* display grid */}
@@ -68,7 +91,7 @@ export default function Home() {
             <div>
               <img className='mt-[50rem]' src='/assets/logos/vue.svg' />
             </div>
-            <div className='hidden lg:block'>
+            <div className='hidden lg:block overflow-clip'>
               <img className='mt-20 translate-x-10' src='/assets/logos/next.svg' />
               <img className='mt-48' src='/assets/logos/react.svg' />
               <img className='mt-72' src='/assets/logos/svelte_dim.svg' />
@@ -81,9 +104,9 @@ export default function Home() {
               <img className='w-full' src='/assets/squiggly.svg' />
               <div className='w-full bg-white h-96 -mt-2'></div>
             </div>
-            <h1 className='text-[133px] text-white font-black text-center'>api</h1>
-            <p className='max-w-[730px] text-2xl text-white text-center mt-10'>At build time, a Jamstack website uses data from one or more APIs to generate markup. These APIs can be a headless CMS like Prismic, a database like Supabase, or even a local JSON file!</p>
-            <div className="relative inline-block mx-auto mt-40">
+            <h1 className='text-6xl lg:text-[133px] text-white font-black text-center'>api</h1>
+            <p className='max-w-[85%] md:max-w-[730px] lg:text-2xl text-white text-center mt-10 mb-16 lg:mb-40'>At build time, a Jamstack website uses data from one or more APIs to generate markup. These APIs can be a headless CMS like Prismic, a database like Supabase, or even a local JSON file!</p>
+            <div className="relative inline-block mx-auto">
               <img src='/assets/ball.webp' />
               <div className='absolute -inset-2 bg-gradient-to-tr from-blue-600 to-purple-700 -z-10 rounded-full blur-[128px] -translate-y-10 -translate-x-10'></div>
               <div className='absolute inset-0 bg-white -z-10 rounded-full blur-2xl -translate-y-8 -translate-x-8'></div>
@@ -98,7 +121,7 @@ export default function Home() {
       </section>
 
       {/* Footer section */}
-      <div className="bg-brand-bg">
+      <div className="bg-brand-bg px-0 sm:px-5 2xl:px-0">
         <section className='flex flex-col items-center -translate-y-24'>
 
           {/* Display grid */}
@@ -110,28 +133,16 @@ export default function Home() {
           </div>
 
           {/* Panel */}
-          <div className='relative overflow-clip w-[1204px] rounded-[68px] p-10 backdrop-blur-[128px] border border-[#e2ffd125]
+          <div className='relative overflow-clip w-full max-w-[1204px] rounded-[68px] p-10 backdrop-blur-[128px] border border-[#e2ffd125]
           [@supports(backdrop-filter:blur(0))]:backdrop-blur-[128px] [@supports(backdrop-filter:blur(0))]:[background:linear-gradient(-24.39deg,rgba(255,255,255,0.024)_35.54%,rgba(123,245,93,.15)_75.25%)] [background:linear-gradient(#afbdac_-15%,#263822_20%,rgba(255,255,255,.05))]'>
-            <h2 className='mt-24 text-center text-7xl font-black text-[#EFFFE2]'>top audit scores</h2>
-            <div className='grid grid-cols-2 md:grid-cols-4 gap-4 mt-14 mb-24'>
-              <div className='relative'>
-                <img src='/assets/circle.svg' className='scale-125' />
-                <div className='text-6xl text-[#EFFFE2] font-bold absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 drop-shadow-circle'>100</div>
-              </div>
-              <div className='relative'>
-                <img src='/assets/circle.svg' className='scale-125' />
-                <div className='text-6xl text-[#EFFFE2] font-bold absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 drop-shadow-circle'>100</div>
-              </div>
-              <div className='relative'>
-                <img src='/assets/circle.svg' className='scale-125' />
-                <div className='text-6xl text-[#EFFFE2] font-bold absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 drop-shadow-circle'>100</div>
-              </div>
-              <div className='relative'>
-                <img src='/assets/circle.svg' className='scale-125' />
-                <div className='text-6xl text-[#EFFFE2] font-bold absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 drop-shadow-circle'>100</div>
-              </div>
+            <h2 className='md:mt-12 lg:mt-20 2xl:mt-24 text-center text-5xl sm:text-6xl lg:text-7xl font-black text-[#EFFFE2]'>top audit scores</h2>
+            <div className='grid grid-cols-2 sm:grid-cols-4 sm:gap-4 mt-10 lg:mt-14 mb-16 lg:mb-24'>
+              <Circle />
+              <Circle />
+              <Circle />
+              <Circle />
             </div>
-            <p className='relative mx-auto text-[26px] max-w-3xl text-transparent leading-10 mb-48
+            <p className='relative mx-auto text-lg md:text-2xl lg:text-[26px] max-w-3xl text-transparent lg:leading-10 mb-24 lg:mb-48
             bg-gradient-to-br from-[#c5ffc0] to-white/80 bg-clip-text'>
               Search engines, business stakeholders, and end-users gauge a website’s value using metric tools like Google Lighthouse, which measures site performance, SEO, and accessibility.<br /><br />Performance is one of the most difficult scores to get to 100. This is especially true in a time when users demand JavaScript-heavy, highly-interactive web experiences.<br /><br />A Jamstack website, delivering SEO-friendly, lean {'&'} clean HTML in milliseconds, promises higher search-engine ranking and a more positive, responsive end-user experience.</p>
           </div>
@@ -145,3 +156,12 @@ export default function Home() {
     </>
   );
 };
+
+function Circle() {
+  return <div className='relative'>
+    <img src='/assets/circle.svg' className='scale-125' />
+    <div className='text-4xl md:text-5xl lg:text-6xl text-[#EFFFE2] font-bold absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 drop-shadow-circle'>
+      100
+    </div>
+  </div>;
+}
