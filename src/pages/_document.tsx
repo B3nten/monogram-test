6275f9943d80e1c9271f64e7e5ff4e2c341c8325
benten <ben@benten.ca>
2022-06-13 18:12:28 -0600

    Change favicon and title before I forget

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com" rel="preconnect" />
           <link href="https://fonts.gstatic.com" rel="preconnect" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&display=swap" rel="stylesheet" />
+          <title>Monogram Test - Benton</title>
         </Head>
         <body>
           <Main />
