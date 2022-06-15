ab722939d3dc17a0573520626c3b1d0b446c4d65
benten <ben@benten.ca>
2022-06-14 22:06:46 -0600

    Model.tsx ignore typeerrors (unsafe)

--- src/modules/index/Ball/Model.tsx
+++ src/modules/index/Ball/Model.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
-
+//@ts-ignore
 export function Model(props) {
     const { viewport } = useThree();
     const group = useRef();
     useEffect(() => {
         const x = innerWidth / 2;
         const y = innerHeight / 2;
+        //@ts-ignore
         function foo(e) {
             const mouseX = (e.clientX - x) / x;
             const mouseY = (e.clientY - y) / x;
+            //@ts-ignore
             group.current.rotation.x = (mouseY * .3) + .6;
+            //@ts-ignore
             group.current.rotation.y = (mouseX * .2) - 1;
         }
         document.addEventListener('mousemove', foo);
@@ -19,7 +22,7 @@ export function Model(props) {
             removeEventListener('mousemove', foo);
         };
     }, [innerHeight, innerWidth]);
-
+    //@ts-ignore
     const { nodes, materials } = useGLTF("/assets/models/Ball.glb");
     return (
         <group rotation={[.6, -1, .8]} ref={group} {...props} dispose={null}>
